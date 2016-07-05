65d4101a81f2cc5b66eb51c177626c13c65fb1cd
cezres <cezr@sina.com>
2016-07-05 19:27:07 +0800

    TabBar

---
+++ bilibili fake/Class/Common/Views/TabBar/TabBar.h
@@ -0,0 +1,15 @@
+//
+//  TabBar.h
+//  bilibili fake
+//
+//  Created by 翟泉 on 2016/7/5.
+//  Copyright © 2016年 云之彼端. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TabBar : UIView
+
+- (instancetype)initWithTitles:(NSArray<NSString *> *)titles;
+
+@end
---
+++ bilibili fake/Class/Common/Views/TabBar/TabBar.m
@@ -0,0 +1,20 @@
+//
+//  TabBar.m
+//  bilibili fake
+//
+//  Created by 翟泉 on 2016/7/5.
+//  Copyright © 2016年 云之彼端. All rights reserved.
+//
+
+#import "TabBar.h"
+
+@implementation TabBar
+
+- (instancetype)initWithTitles:(NSArray<NSString *> *)titles; {
+    if (self = [super init]) {
+        
+    }
+    return self;
+}
+
+@end
