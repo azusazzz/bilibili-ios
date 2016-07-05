7cfa0cd781c9a19ec807dd38c6a296d1e84b9769
cezres <cezr@sina.com>
2016-07-05 20:29:09 +0800

    首页接口整理

---
+++ bilibili fake/Class/Modules/Home/View/HomeAnimationView.m
@@ -0,0 +1,21 @@
+//
+//  HomeAnimationView.m
+//  bilibili fake
+//
+//  Created by 翟泉 on 2016/7/5.
+//  Copyright © 2016年 云之彼端. All rights reserved.
+//
+
+#import "HomeAnimationView.h"
+
+@implementation HomeAnimationView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
