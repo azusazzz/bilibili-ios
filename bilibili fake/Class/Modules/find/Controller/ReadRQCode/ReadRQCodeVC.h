4964d41b370d1317f4a6483c88b9dc6dd4a27d20
caixuehao <cxh@cxhdeMac-mini.local>
2016-07-06 11:53:19 +0800

    pdxproj提交2016.07.06   11:53

---
+++ bilibili fake/Class/Modules/find/Controller/ReadRQCode/ReadRQCodeVC.h
@@ -0,0 +1,13 @@
+//
+//  ReadRQCodeVC.h
+//  bilibili fake
+//
+//  Created by cxh on 16/7/6.
+//  Copyright © 2016年 云之彼端. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReadRQ_CodeVC : UIViewController
+
+@end
---
+++ bilibili fake/Class/Modules/find/Controller/ReadRQCode/ReadRQCodeVC.m
@@ -0,0 +1,13 @@
+//
+//  ReadRQCodeVC.m
+//  bilibili fake
+//
+//  Created by cxh on 16/7/6.
+//  Copyright © 2016年 云之彼端. All rights reserved.
+//
+
+#import "ReadRQCodeVC.h"
+
+@implementation ReadRQ_CodeVC
+
+@end
