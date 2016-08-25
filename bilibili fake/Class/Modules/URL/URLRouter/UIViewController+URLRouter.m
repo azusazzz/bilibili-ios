40c39208ae0667ffcec6fd1bd0e97705047cc767
cezres <cezr@sina.com>
2016-08-25 21:46:34 +0800

    -

---
+++ bilibili fake/Class/Modules/URL/URLRouter/UIViewController+URLRouter.m
@@ -0,0 +1,20 @@
+//
+//  UIViewController+URLRouter.m
+//  bilibili fake
+//
+//  Created by 翟泉 on 2016/8/25.
+//  Copyright © 2016年 云之彼端. All rights reserved.
+//
+
+#import "UIViewController+URLRouter.h"
+
+//#pragma clang diagnostic push
+//#pragma clang diagnostic ignored "-Wprotocol"
+
+@implementation UIViewController (URLRouter)
+
+
+@end
+
+//#pragma clang diagnostic pop
+
