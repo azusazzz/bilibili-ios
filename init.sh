b37c673c71b729e44974ca6dff3ad93c3d9989ea
云之彼端 <cezres@163.com>
2016-11-29 13:50:38 +0800

    -

--- init.sh
+++ init.sh
@@ -2,26 +2,24 @@
 
 function download() {
 
-if [ ! -f shellTemp/$1 ];
+if [ ! -f $2/$1 ];
 then
 echo "正在下载${1}..."
-curl "http://oh32pp4u5.bkt.clouddn.com/${1}" --create-dirs -o shellTemp/$1
+curl "http://oh32pp4u5.bkt.clouddn.com/${1}" --create-dirs -o $2/$1
 fi
 
-if [ ! -f shellTemp/$1 ];
+if [ ! -f $2/$1 ];
 then
 echo "下载失败: ${1}"
 exit -1
 fi
 
-mkdir -p $2
-
-unzip -o shellTemp/$1 -d $2
+unzip -o $2/$1 -d $2
 
 }
 
-download "ijkplayer-k0.7.4.zip" "bilibili/Librarys" "IJKMediaFramework.framework"
-download "wax.zip" "bilibili/Librarys" "wax.framework"
-rm -r bilibili/Librarys/__MACOSX
+download "ijkplayer-k0.7.4.zip" "bilibili/Shell" "IJKMediaFramework.framework"
+download "wax.zip" "bilibili/Shell" "wax.framework"
+rm -r bilibili/Shell/__MACOSX
 
 
